5f96fa19dd6cce881df3a73e11d9f238e700684b
Devon Gaston <kendgast@ut.utm.edu>
2016-08-30 16:54:03 -0500

    Basic Addition

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@
 
 #include <iostream>
 using namespace std;
+
+int sum(int data);
+
 int main(){
-  cout << "Hello World!" << endl;
+  int data;
+  cout << "Please enter a number: ";
+  cin >> data;
+  cout << sum(data) << endl;
   return 0;
+}
+
+int sum(int data){
+  int count = 0;
+  for(int i = 1; i <= data; i++){
+    count = count + i;
+    cout << "Count is now " << count << endl;
+  }
+  return count;
 }
